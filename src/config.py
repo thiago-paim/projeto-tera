8bf7b6dc5f158bba5ee42b28dd96029fed818f31
Thiago Paim <thpaim@gmail.com>
2023-03-30 20:44:45 -0300

    Reestruturação das pastas do projeto (#2)
    * reestruturando projeto
    
    * testando arquitetura de pipeline
    
    * melhorando docs

---
+++ src/config.py
@@ -0,0 +1,5 @@
+import os
+
+DATASETS_PATH = os.path.join(os.getcwd(), 'data')
+RAW_DATASETS_PATH = os.path.join(os.getcwd(), 'data', 'raw')
+PROCESSED_DATASETS_PATH = os.path.join(os.getcwd(), 'data', 'processed')
---
+++ src/utils.py
@@ -0,0 +1,16 @@
+import pandas as pd
+from config import PROCESSED_DATASETS_PATH, RAW_DATASETS_PATH, DATASETS_PATH
+
+
+def load_raw_dataset(file_name: str, sep: str=';') -> pd.DataFrame:
+    df = pd.read_csv(f"{RAW_DATASETS_PATH}/{file_name}", sep=sep)
+    return df
+
+
+def load_processed_dataset(file_name: str, sep: str=';') -> pd.DataFrame:
+    df = pd.read_csv(f"{PROCESSED_DATASETS_PATH}/{file_name}", sep=sep)
+    return df
+
+
+def save_dataset(df: pd.DataFrame, file_name: str, sep: str=';') -> None:
+    df.to_csv(f"{PROCESSED_DATASETS_PATH}/{file_name}", sep=sep, index=False)
