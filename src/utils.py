bb024a4fe4c9a7c82d8fb3f4d0e18a780dfa54ab
Thiago Paim <thpaim@gmail.com>
2023-03-30 21:21:37 -0300

    move drop_duplicated_rows para utils

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from config import PROCESSED_DATASETS_PATH, RAW_DATASETS_PATH, DATASETS_PATH
+from .config import PROCESSED_DATASETS_PATH, RAW_DATASETS_PATH
 
 
 def load_raw_dataset(file_name: str, sep: str=';') -> pd.DataFrame:
@@ -14,3 +14,9 @@ def load_processed_dataset(file_name: str, sep: str=';') -> pd.DataFrame:
 
 def save_dataset(df: pd.DataFrame, file_name: str, sep: str=';') -> None:
     df.to_csv(f"{PROCESSED_DATASETS_PATH}/{file_name}", sep=sep, index=False)
+
+
+def drop_duplicated_rows(df: pd.DataFrame) -> pd.DataFrame:
+    duplicated_indexes = df[df.duplicated()].index
+    df = df.drop(duplicated_indexes)
+    return df
