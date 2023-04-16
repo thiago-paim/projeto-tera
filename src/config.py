83886a76105acc6937f7b746bbaeefc0acf54247
Thiago Paim <thpaim@gmail.com>
2023-04-15 21:44:47 -0300

    configuracoes

--- src/config.py
+++ src/config.py
@@ -1,5 +1,9 @@
 import os
+from decouple import config
 
 
 RAW_DATASETS_PATH = os.path.join(os.getcwd(), "data", "raw")
 PROCESSED_DATASETS_PATH = os.path.join(os.getcwd(), "data", "processed")
+
+OPENAI_API_KEY = config("OPENAI_API_KEY")
+PREFECT_API_KEY = config("PREFECT_API_KEY")
