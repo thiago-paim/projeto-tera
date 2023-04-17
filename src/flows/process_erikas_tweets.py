db2c590972c55f4a63d411603d0c841d8a6426bc
Thiago Paim <thpaim@gmail.com>
2023-04-17 12:42:43 -0300

    pequenas correcoes a atualizacoes de nome

--- src/flows/process_erikas_tweets.py
+++ src/flows/process_erikas_tweets.py
@@ -3,7 +3,11 @@ import pandas as pd
 from prefect import flow, task, get_run_logger
 from src.common import load_raw_dataset
 from src.feature_extraction import bag_of_words
-from src.filters import drop_duplicated_rows, drop_unused_tweet_columns, get_tweets_by_username
+from src.filters import (
+    drop_duplicated_rows,
+    drop_unused_tweet_columns,
+    get_tweets_by_username,
+)
 from src.preprocess import filter_letters, remove_stopwords, lemmatize
 from src.models import topic_modelling_sentiment_analysis
 
@@ -12,16 +16,16 @@ from src.models import topic_modelling_sentiment_analysis
 def apply_filters(df: pd.DataFrame) -> pd.DataFrame:
     # Removendo colunas pouco informativas
     df = drop_unused_tweet_columns(df)
-    
+
     # Remove linhas com tweets duplicados
     df = drop_duplicated_rows(df)
-    
+
     # Filtra somente tweets do usuario
-    username = 'https://twitter.com/ErikakHilton'
+    username = "https://twitter.com/ErikakHilton"
     df = get_tweets_by_username(df, username)
     return df
 
- 
+
 @task
 def preprocess(tweets: pd.DataFrame) -> pd.DataFrame:
     def pre_process(text: str):
@@ -49,21 +53,21 @@ def run_model(df: pd.DataFrame) -> pd.DataFrame:
 def process_erikas_tweets():
     logger = get_run_logger()
     logger.info("Starting Process Erikas Tweets flow")
-    
-    file_name = 'ErikakHilton-2022-10-01-2022-11-01-2023-02-16T19:31:01.224464.csv'
+
+    file_name = "ErikakHilton-tweets.csv"
     log_param("file_name", file_name)
     df = load_raw_dataset(file_name)
-    
+
     tweets = apply_filters(df)
     tweets = preprocess(tweets)
     X = extract_features(tweets)
     results = run_model(X)
-    
-    pos_class = results[results['classif_tmba'] == 'positive'].shape[0]
-    neg_class = results[results['classif_tmba'] == 'negative'].shape[0]
+
+    pos_class = results[results["classif_tmba"] == "positive"].shape[0]
+    neg_class = results[results["classif_tmba"] == "negative"].shape[0]
     log_metric("pos_class", pos_class)
     log_metric("neg_class", neg_class)
-    
+
     return results
 
 
