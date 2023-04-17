8ae2c6036e790e70353d63fe81b4d774d194b42c
Thiago Paim <thpaim@gmail.com>
2023-04-17 11:13:25 -0300

    experimentos com api do ChatGPT

--- src/feature_extraction.py
+++ src/feature_extraction.py
@@ -1,9 +1,73 @@
 import os
 import ast
 import re
+import openai
 import pandas as pd
 from scipy.sparse._csr import csr_matrix
 from sklearn.feature_extraction.text import CountVectorizer
+
+from src.config import OPENAI_API_KEY
+
+openai.api_key = OPENAI_API_KEY
+
+
+def get_chatgpt_sentiment_analysis(text: str) -> dict:
+    base_msg = """
+        Você irá realizar uma análise de sentimentos em um tweet que irei mandar.
+        Sua resposta deve vir como um dicionario Python de três chaves: 'sentiment', 'value' e 'comment', com o seguinte significado:
+        - sentiment: Positivo, Neutro ou Negativo
+        - value: Um valor de -1 (Negativo) a 1 (Positivo)
+        - comment: Uma frase explicando o motivo da sua classificação
+        Por exemplo: {
+            'sentiment': 'Positivo',
+            'value': 0.9,
+            'comment': 'O tweet expressa felicidade devido a uma boa notícia'
+        }
+        
+        O tweet é:
+    """
+    msg = base_msg + text
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[{"role": "user", "content": msg}],
+        temperature=0,
+    )
+    response_content = response["choices"][0]["message"]["content"]
+    return ast.literal_eval(response_content)
+
+
+# def get_chatgpt_sentiment_analysis_column(
+#     df: pd.DataFrame,
+#     text_column: str = "content",
+#     new_columns: list = ["gpt_class", "gpt_value", "gpt_comment"],
+#     limit: int = 10,
+#     tweets_per_request: int = 5,
+# ) -> pd.DataFrame:
+#     """Adiciona uma nova coluna ao DataFrame com a análise de sentimento do ChatGPT"""
+
+#     base_msg = """
+#         Você irá realizar análise de sentimentos em uma série de tweets que mandarei.
+#         Para cada tweet voce deve gerar um dicionario Python de três chaves: 'sentiment', 'value' e 'comment', com o seguinte significado:
+#         - sentiment: Positivo, Neutro ou Negativo
+#         - value: Um valor de -1 (Negativo) a 1 (Positivo)
+#         - comment: Uma frase explicando o motivo da sua classificação
+#         Por exemplo: {
+#             'sentiment': 'Positivo',
+#             'value': 0.9,
+#             'comment': 'O tweet expressa felicidade devido a uma boa notícia'
+#         }
+#         A sua resposta deve vir no formato de uma lista com os dicionários gerados.
+#         Seguem abaixo os tweets:
+
+#     """
+
+#     response = openai.ChatCompletion.create(
+#         model="gpt-3.5-turbo",
+#         messages=[{"role": "user", "content": base_msg}],
+#         temperature=0,
+#     )
+#     response_content = response["choices"][0]["message"]["content"]
+#     print(response_content)
 
 
 def get_twitter_username_by_url(url: str) -> str:
