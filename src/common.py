e45a32e3c8b726702f2122f4acc35429d6f279ce
Thiago Paim <thpaim@gmail.com>
2023-06-19 14:12:35 -0300

    adiciona funcoes gerais para blob

--- src/common.py
+++ src/common.py
@@ -1,5 +1,43 @@
+from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
 import pandas as pd
-from src.config import PROCESSED_DATASETS_PATH, RAW_DATASETS_PATH, PROJECT_PATH
+from src.config import (
+    AZURE_STORAGE_ACCOUNT_KEY,
+    AZURE_STORAGE_CONTAINER,
+    AZURE_STORAGE_ACCOUNT_URL,
+    PROCESSED_DATASETS_PATH,
+    RAW_DATASETS_PATH,
+    PROJECT_PATH,
+    TEMP_FILES_PATH,
+)
+
+
+def download_blob(file_name: str) -> str:
+    blob_service_client_instance = BlobServiceClient(
+        account_url=AZURE_STORAGE_ACCOUNT_URL, credential=AZURE_STORAGE_ACCOUNT_KEY
+    )
+    blob_client_instance = blob_service_client_instance.get_blob_client(
+        AZURE_STORAGE_CONTAINER, file_name, snapshot=None
+    )
+
+    temp_file_path = f"{TEMP_FILES_PATH}/{file_name}"
+    with open(temp_file_path, "wb") as blob_file:
+        blob_data = blob_client_instance.download_blob()
+        blob_data.readinto(blob_file)
+
+    return temp_file_path
+
+
+def upload_blob(df, file_name: str, sep=";"):
+    temp_file_path = f"{TEMP_FILES_PATH}/{file_name}"
+    df.to_csv(f"{temp_file_path}", sep=sep, encoding="utf-8", index=False)
+
+    service_client = BlobServiceClient(
+        account_url=AZURE_STORAGE_ACCOUNT_URL, credential=AZURE_STORAGE_ACCOUNT_KEY
+    )
+    container_client = service_client.get_container_client(AZURE_STORAGE_CONTAINER)
+
+    with open(file=temp_file_path, mode="rb") as data:
+        container_client.upload_blob(name=file_name, data=data, overwrite=True)
 
 
 def load_dataset(file_path: str, sep: str = ";") -> pd.DataFrame:
--- src/config.py
+++ src/config.py
@@ -5,6 +5,10 @@ from decouple import config
 PROJECT_PATH = os.path.join(os.getcwd())
 RAW_DATASETS_PATH = os.path.join(os.getcwd(), "data", "raw")
 PROCESSED_DATASETS_PATH = os.path.join(os.getcwd(), "data", "processed")
+TEMP_FILES_PATH = os.path.join(os.getcwd(), "temp")
 
+AZURE_STORAGE_ACCOUNT_URL = "https://projetotera.blob.core.windows.net"
+AZURE_STORAGE_CONTAINER = "datasets"
+AZURE_STORAGE_ACCOUNT_KEY = config("AZURE_STORAGE_ACCOUNT_KEY")
 OPENAI_API_KEY = config("OPENAI_API_KEY")
 PREFECT_API_KEY = config("PREFECT_API_KEY")
