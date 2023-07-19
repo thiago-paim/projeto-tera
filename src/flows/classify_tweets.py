14ac6454be9d76a9905e7ac77c3fc92d54ca4e80
Thiago Paim <thpaim@gmail.com>
2023-07-19 01:42:30 -0300

    Adicionar flow de classificao de ofensas

--- src/flows/classify_tweets.py
+++ src/flows/classify_tweets.py
@@ -1,8 +1,10 @@
+from datetime import datetime
 import sys
 from time import time
 import pandas as pd
 import prefect
 from prefect import flow, task, get_run_logger
+import pytz
 from src.common import (
     download_blob,
     upload_blob,
@@ -14,44 +16,97 @@ import torch
 from tqdm import tqdm
 
 
+OFFENSE_MODELS = {
+    "ru_bert_base": "ruanchaves/bert-base-portuguese-cased-hatebr",
+    "ru_bert_large": "ruanchaves/bert-large-portuguese-cased-hatebr",
+    "ru_mdeberta_base": "ruanchaves/mdeberta-v3-base-hatebr",
+    "ci_distilbert_base": "citizenlab/distilbert-base-multilingual-cased-toxicity",
+}
+
+
 @task
-def classify(
-    df: pd.DataFrame, col_name: str = "content", batch_size: int = 50
-) -> pd.DataFrame:
+def filter_tweets(df: pd.DataFrame, date_col: str = "date") -> pd.DataFrame:
+    local_tz = pytz.timezone("America/Sao_Paulo")
+    since = datetime(year=2022, month=9, day=1, tzinfo=local_tz)
+    until = datetime(year=2022, month=11, day=1, tzinfo=local_tz)
+
+    df[date_col] = pd.to_datetime(df[date_col])
+
+    df = df[(df[date_col] >= since) & (df[date_col] <= until)]
+
+    # df = df.iloc[:100]
+    return df
+
+
+@task
+def classify(df: pd.DataFrame, col_name: str, batch_size: int = 32) -> pd.DataFrame:
     logger = get_run_logger()
-    model_name = "ruanchaves/bert-large-portuguese-cased-hatebr"
 
     tqdm.pandas()
-
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-    if device.type == "cuda":
-        classifier = pipeline("sentiment-analysis", model=model_name, device=0)
-    else:
-        classifier = pipeline("sentiment-analysis", model=model_name)
-
-    t1 = time()
     num_batches = len(df) // batch_size + 1
-    logger.info(
-        f"\nStarting classification: {df.shape=}, {batch_size=}, {num_batches=}, {device=}"
-    )
+    results = {}
+    for model_key, model_name in OFFENSE_MODELS.items():
+        if device.type == "cuda":
+            classifier = pipeline("sentiment-analysis", model=model_name, device=0)
+        else:
+            classifier = pipeline("sentiment-analysis", model=model_name)
 
-    results = []
-    for i in tqdm(range(num_batches)):
-        batch_start = i * batch_size
-        batch_end = min((i + 1) * batch_size, len(df))
-        batch_texts = df[col_name][batch_start:batch_end].tolist()
-        batch_results = classifier(batch_texts)
-        results += batch_results
+        results[model_key] = []
+        t1 = time()
+        logger.info(
+            f"\nStarting classification: {model_name=}, {df.shape=}, {batch_size=}, {num_batches=}, {device=}"
+        )
+        for i in tqdm(range(num_batches)):
+            batch_start = i * batch_size
+            batch_end = min((i + 1) * batch_size, len(df))
+            batch_texts = df[col_name][batch_start:batch_end].tolist()
+            batch_results = classifier(batch_texts)
+            results[model_key] += batch_results
 
-    t2 = time()
-    logger.info(
-        f"Finished classification in {t2-t1:.3f} seconds; {df.shape=}, {batch_size=}, {num_batches=}, {device=}"
-    )
+        t2 = time()
+        logger.info(
+            f"Finished classification in {t2-t1:.3f} seconds; {df.shape=}, {batch_size=}, {num_batches=}, {device=}"
+        )
 
-    df["class_label"] = [result["label"] for result in results]
-    df["class_score"] = [result["score"] for result in results]
+    for key in OFFENSE_MODELS.keys():
+        print(key)
+        df[f"{key}_label"] = [result["label"] for result in results[key]]
+        df[f"{key}_score"] = [result["score"] for result in results[key]]
 
+    return df
+
+
+@task
+def clean_outputs(df: pd.DataFrame) -> pd.DataFrame:
+    def get_distilbert_label(label):
+        if label == "toxic":
+            return True
+        return False
+
+    if "ci_distilbert_base_label" in df.columns:
+        df["ci_distilbert_base_label"] = df["ci_distilbert_base_label"].apply(
+            get_distilbert_label
+        )
+
+    return df
+
+
+@task
+def get_label_sum(df: pd.DataFrame) -> pd.DataFrame:
+    def get_offense_label_sum(row):
+        count = 0
+        if row["ru_bert_base_label"] == True:
+            count += 1
+        if row["ru_bert_large_label"] == True:
+            count += 1
+        if row["ru_mdeberta_base_label"] == True:
+            count += 1
+        if row["ci_distilbert_base_label"] == True:
+            count += 1
+        return count
+
+    df["label_sum"] = df.apply(get_offense_label_sum, axis=1)
     return df
 
 
@@ -64,7 +119,9 @@ def cleanup(file_name):
 
 @flow(name="Classify Tweets")
 def classify_tweets(
-    file_name: str = "erika-short.csv", source: str = "azure", col_name: str = "content"
+    file_name: str = "sp_elected_state_deputies_tweets.csv",
+    source: str = "azure",
+    col_name: str = "content",
 ):
     flow_name = prefect.context.get_run_context().flow.name
     flow_params = prefect.context.get_run_context().flow_run.dict()["parameters"]
@@ -76,9 +133,12 @@ def classify_tweets(
         df = pd.read_csv(file_path, sep=";")
 
     if source == "local":
-        df = load_processed_dataset(file_name, sep=",")
+        df = load_processed_dataset(file_name, sep=";")
 
+    df = filter_tweets(df)
     df = classify(df, col_name=col_name)
+    df = clean_outputs(df)
+    df = get_label_sum(df)
 
     if source == "azure":
         upload_blob(df, file_name)
@@ -91,3 +151,10 @@ def classify_tweets(
 
 if __name__ == "__main__":
     classify_tweets(*sys.argv[1:])
+
+
+"""To Do:
+- Criar um dicionario de códigos por modelo, para facilitar a visualização
+- Adicionar todos os modelos no `classify`, em colunas novas
+- Otimizar execução em GPU
+"""
